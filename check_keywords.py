50a574d0c0bf4a64230268a392317111ae99b58b
xiaobing-123 <602104965@qq.com>
2023-04-19 09:28:19 +0800

    test

--- check_keywords.py
+++ check_keywords.py
@@ -1,25 +1,27 @@
-import os
 import sys
+import os
 
-# 要检查的关键字列表
-KEYWORDS = ['sensetime', 'sensecore', 'SenseTime']
+KEYWORDS = ["phantomjs.exe", "another_keyword"]
 
-def check_file(filename):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            for keyword in KEYWORDS:
-                if keyword in line:
-                    print(f'Error: {keyword} found in {filename}, line {i+1}')
-                    sys.exit(1)
 
-def check_dir(dirname):
-    for root, dirs, files in os.walk(dirname):
-        for filename in files:
-            if filename == 'check_keywords.py':
-                continue
-            if filename.endswith('.py'):
-                check_file(os.path.join(root, filename))
+def check_file(file_path):
+    _, extension = os.path.splitext(file_path)
 
-if __name__ == '__main__':
-    check_dir('.')
+    if extension == ".py":
+        # 如果是 Python 文件，则使用 Python 解释器执行文件以检查关键字
+        with open(file_path, "r") as f:
+            file_contents = f.read()
+        for keyword in KEYWORDS:
+            if keyword in file_contents:
+                print(f"{file_path}: {keyword} found!")
+    else:
+        # 如果不是 Python 文件，则直接使用 grep 命令检查关键字
+        cmd = f"grep -q {'|'.join(KEYWORDS)} {file_path}"
+        return_code = os.system(cmd)
+        if return_code == 0:
+            print(f"{file_path}: {' or '.join(KEYWORDS)} found!")
+
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    check_file(file_path)

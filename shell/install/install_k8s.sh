6130f3bb4d9b52218d8b9a9964dd01aaaa711cfb
lwj2048 <602104965@qq.com>
2025-09-03 14:14:56 +0800

    Update install_k8s.sh

--- shell/install/install_k8s.sh
+++ shell/install/install_k8s.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/lwj-st/kubesphere-install.git
 cd kubesphere-install
 
-export KKZONE=cn
+# export KKZONE=cn
 echo yes|./kk create cluster --with-kubernetes v1.21.5  --with-kubesphere v3.3.2
